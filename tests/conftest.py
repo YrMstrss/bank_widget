c1937de0f7579905d7a98210737f1013fa604692
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-04 15:30:58 +0300

    add test for sort_operations func

--- tests/conftest.py
+++ tests/conftest.py
@@ -129,3 +129,48 @@ def executed_data():
     "description": "Открытие вклада",
     "to": "Счет 90424923579946435907"
   }]
+
+
+@pytest.fixture
+def sorted_data():
+    return [{
+    "id": 863064926,
+    "state": "EXECUTED",
+    "date": "2019-12-08T22:46:21.935582",
+    "operationAmount": {
+      "amount": "41096.24",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 90424923579946435907"
+  },{
+    "id": 596171168,
+    "state": "EXECUTED",
+    "date": "2018-07-11T02:26:18.671407",
+    "operationAmount": {
+      "amount": "79931.03",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 72082042523231456215"
+  },{
+    "id": 716496732,
+    "state": "EXECUTED",
+    "date": "2018-04-04T17:33:34.701093",
+    "operationAmount": {
+      "amount": "40701.91",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Visa Gold 5999414228426353",
+    "to": "Счет 72731966109147704472"
+  }]
--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,4 +1,4 @@
-from src.funcs import get_data_from_json, get_executed_operations
+from src.funcs import get_data_from_json, get_executed_operations, sort_operations
 
 
 def test_get_data_from_json(file, data):
@@ -6,3 +6,7 @@ def test_get_data_from_json(file, data):
 
 def test_get_executed_operations(data,executed_data):
     assert get_executed_operations(data) == executed_data
+
+
+def test_sort_operations(executed_data, sorted_data):
+    assert sort_operations(executed_data) == sorted_data
