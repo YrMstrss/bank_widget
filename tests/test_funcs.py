aa4836f920d3338a34302b0f6c62c026f976dc41
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-04 15:32:54 +0300

    add test for format_date func

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,4 +1,4 @@
-from src.funcs import get_data_from_json, get_executed_operations, sort_operations
+from src.funcs import get_data_from_json, get_executed_operations, sort_operations, format_date
 
 
 def test_get_data_from_json(file, data):
@@ -10,3 +10,7 @@ def test_get_executed_operations(data,executed_data):
 
 def test_sort_operations(executed_data, sorted_data):
     assert sort_operations(executed_data) == sorted_data
+
+
+def test_format_date():
+    assert format_date('2019-12-08T22:46:21.935582') == '08.12.2019'
