8cb154b9e58a20975f31de33f7614e52949a6a54
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-04 15:03:26 +0300

    add test_get_executed_operations

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,5 +1,8 @@
-from src.funcs import get_data_from_json
+from src.funcs import get_data_from_json, get_executed_operations
 
 
 def test_get_data_from_json(file, data):
     assert get_data_from_json(file) == data
+
+def test_get_executed_operations(data,executed_data):
+    assert get_executed_operations(data) == executed_data
