c8c743ac59a6d6441fef2137363821a9ffd1d33f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-04 15:02:57 +0300

    update test.json and conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,6 @@ def file():
 def data():
     return  [{
     "id": 895315941,
-    "state": "EXECUTED",
     "date": "2018-08-19T04:27:37.904916",
     "operationAmount": {
       "amount": "56883.54",
@@ -81,4 +80,52 @@ def data():
       "description": "Перевод организации",
       "from": "Visa Platinum 1246377376343588",
       "to": "Счет 14211924144426031657"
+  }]
+
+
+@pytest.fixture
+def executed_data():
+    return  [
+  {
+    "id": 596171168,
+    "state": "EXECUTED",
+    "date": "2018-07-11T02:26:18.671407",
+    "operationAmount": {
+      "amount": "79931.03",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 72082042523231456215"
+  },
+  {
+    "id": 716496732,
+    "state": "EXECUTED",
+    "date": "2018-04-04T17:33:34.701093",
+    "operationAmount": {
+      "amount": "40701.91",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Visa Gold 5999414228426353",
+    "to": "Счет 72731966109147704472"
+  },
+  {
+    "id": 863064926,
+    "state": "EXECUTED",
+    "date": "2019-12-08T22:46:21.935582",
+    "operationAmount": {
+      "amount": "41096.24",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 90424923579946435907"
   }]
